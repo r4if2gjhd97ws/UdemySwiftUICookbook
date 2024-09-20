f37d1490e8c58d1ca86fd7c8d6e55e2cd6c55dd7
yohei <>
2024-09-20 20:27:24 +0900

    プロジェクト追加

---
+++ UdemySwiftUICookbook/UdemySwiftUICookbook/UdemySwiftUICookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemySwiftUICookbookApp.swift
+//  UdemySwiftUICookbook
+//
+//  Created by yohei on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct UdemySwiftUICookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UdemySwiftUICookbook/UdemySwiftUICookbookTests/UdemySwiftUICookbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdemySwiftUICookbookTests.swift
+//  UdemySwiftUICookbookTests
+//
+//  Created by yohei on 2024/09/20.
+//
+
+import Testing
+@testable import UdemySwiftUICookbook
+
+struct UdemySwiftUICookbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
